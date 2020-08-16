f46e6ae983783ecd594e0b303d75b5493c438bce
Mau Abata <mauabata@gmail.com>
2020-08-16 17:58:26 -0500

    Updates

---
+++ precache-manifest.2fd35a962051a1c00edf025befbc9808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae3404d241a0711dfae8d3ec9b0eeb0",
+    "url": "/nogasm-ui/index.html"
+  },
+  {
+    "revision": "f5c1faf309d295db5a64",
+    "url": "/nogasm-ui/static/css/main.2321a0bf.chunk.css"
+  },
+  {
+    "revision": "ed02191d6b876516571a",
+    "url": "/nogasm-ui/static/js/2.66e00edc.chunk.js"
+  },
+  {
+    "revision": "406904863c710d49aa6b90ffccf78c81",
+    "url": "/nogasm-ui/static/js/2.66e00edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c1faf309d295db5a64",
+    "url": "/nogasm-ui/static/js/main.752bcff1.chunk.js"
+  },
+  {
+    "revision": "34c67cecbdbdf68eae6e",
+    "url": "/nogasm-ui/static/js/runtime-main.7c45ce5c.js"
+  }
+]);
