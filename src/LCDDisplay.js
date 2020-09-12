279a0b918421a15fad63becbb99566c44ab2c9c9
Mau Abata <mauabata@gmail.com>
2020-09-11 20:08:39 -0500

    Working screnshot viewer!!

--- src/LCDDisplay.js
+++ src/LCDDisplay.js
@@ -1,37 +1,53 @@
 import React from 'react';
 
-const LCDDisplay = ({data}) => {
+const LCDDisplay = ({data = ""}) => {
   const width = 128;
   const height = 64;
   const colHeight = Math.floor((height + 7) / 8);
 
-  console.log({ data});
+  let pages = [];
 
-  let columns = [];
-  for(let c = 0; c < width; c++) {
-    let rows = [];
-    const start = c * colHeight;
-    const colData = data.slice(start, start + (colHeight * 2));
-    const colHex = colData.match(/..?/g) || [];
-    const colBytes = colHex.map((i) => parseInt(i, 16));
-    console.log({ colData, colHex, colBytes });
+  for (let page = 0; page < height / colHeight; page++) {
+    const pageStart = page * width * 2; // 128 bytes per page
+    const pageData = data.slice(pageStart, pageStart + (width * 2));
+    const pageDataHex = pageData.match(/..?/g) || [];
+    const pageDataBytes = pageDataHex.map((i) => parseInt(i, 16));
 
-    for (let r = 0; r < height; r++) {
-      const byte = colBytes[Math.floor(r/8)];
-      const bit = !!(byte & (1 << 7-(r % 8)));
-      console.log({c, r, colBytes, byte, bit});
-      rows.push(<div key={"c" + c + "r" + r} className={'pixel'} style={{ border: "1px solid black", backgroundColor: (bit ? "green" : "black"), width: 2, height: 2 }} />);
+    let columns = [];
+
+    for (let column = 0; column < width; column++) {
+      let rows = [];
+      let byte = pageDataBytes[column];
+
+      for (let row = 0; row < 8; row++) {
+        const bit = !!(byte & (1 << (7 - (row % 8))));
+        rows.push(<div key={"p" + page + "c" + column + "r" + row} className={'pixel'} style={{
+          border: "1px solid black",
+          borderTop: "none",
+          borderLeft: "none",
+          backgroundColor: (bit ? "green" : "black"),
+          width: 3,
+          height: 3
+        }}/>);
+      }
+
+      columns.unshift(<div key={"p" + page + "c" + column} className={'column'} style={{float: "left"}}>
+        {rows}
+      </div>);
     }
 
-    columns.push(<div key={"c" + c} className={'column'} style={{ float: "left" }}>
-      { rows }
-    </div>);
+    pages.unshift(<div key={"p" + page}>{columns}</div>);
   }
 
   return (
-    <div className={'lcd'} style={{ backgroundColor: 'black', display: 'inline-block', width: 'auto'}}>
-      { columns }
-      <br style={{ clear: "both" }} />
+    <div className={'lcd'} style={{
+      backgroundColor: 'black',
+      borderTop: "1px solid black",
+      borderLeft: "1px solid black",
+      display: 'inline-block', width: 'auto'
+    }}>
+      {pages}
+      <br style={{clear: "both"}}/>
     </div>
   );
 };
